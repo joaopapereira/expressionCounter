cd75f7d7ec9768a800c519e2023284d79f26c455
Joao Pereira <joaopapereira@gmail.com>
2015-07-23 18:33:36 -0400

    First test runs successfully

--- src/test/java/uk/co/jpereira/expressionCounter/counter/CounterTest.java
+++ src/test/java/uk/co/jpereira/expressionCounter/counter/CounterTest.java
@@ -1,7 +1,12 @@
 package uk.co.jpereira.expressionCounter.counter;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +18,24 @@ public class CounterTest extends TestCase {
     @Test
     public void testSetMergeList() throws Exception {
         Counter cnt = new Counter();
+        Hashtable<String, List<String>> mergeList = new Hashtable<String, List<String>>();
+        mergeList.put("bamm", new ArrayList<String>());
+        mergeList.put("bamm1", new ArrayList<String>());
 
+        mergeList.get("bamm").add("asd");
+        mergeList.get("bamm").add("asd2");
+        mergeList.get("bamm").add("asd3");
+        mergeList.get("bamm1").add("asd1");
+
+        cnt.setMergeList(mergeList);
+        Assert.assertEquals(mergeList, cnt.getMergeList());
+        mergeList.put("new line", new ArrayList<String>());
+        cnt.setMergeList(mergeList);
+
+        Hashtable retVal = cnt.getMergeList();
+        Assert.assertNotEquals(mergeList, retVal);
+
+        Assert.assertTrue(null != retVal.get("new@@@@@line"));
     }
 
     @Test
