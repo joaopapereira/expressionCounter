9f2406a9265c385661470c10a8a452cb7546ace5
Joao Pereira <joaopapereira@gmail.com>
2015-07-24 18:40:20 -0400

    Added tests for case sensitive and insensitive and ignore list

--- src/test/java/uk/co/jpereira/expressionCounter/counter/CounterTest.java
+++ src/test/java/uk/co/jpereira/expressionCounter/counter/CounterTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
  * Created by Joao Pereira on 23/07/2015.
  */
 public class CounterTest extends TestCase {
-
+    private final String ipsilum = "Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.\n";
     @Test
     public void testSetMergeList() throws Exception {
         Counter cnt = new Counter();
@@ -40,11 +41,75 @@ public class CounterTest extends TestCase {
 
     @Test
     public void testGetResult() throws Exception {
-
+        Counter cnt = new Counter();
+        Hashtable<String, Integer> retVal = new Hashtable<String, Integer>();
+        Assert.assertEquals(retVal, cnt.getResult());
+        retVal.put("Bamm", 1);
+        cnt.setCaseSensitive(true);
+        cnt.calculate("Bamm");
+        Assert.assertEquals(retVal, cnt.getResult());
     }
 
     @Test
-    public void testCalculate() throws Exception {
+    public void testCalculateSimpleExampleCaseSensitive() throws Exception {
+        Counter cnt = new Counter();
+        String text = "Lorem ipsum ad his scripta blandit partiendo. Scripta ad His.";
+        Hashtable<String, Integer> retVal;
+        retVal = cnt.calculate(text);
+        cnt.setCaseSensitive(true);
+        Assert.assertEquals(2, retVal.get("ad").intValue());
+        Assert.assertEquals(1, retVal.get("his").intValue());
+        Assert.assertEquals(1, retVal.get("Scripta").intValue());
+    }
+    @Test
+    public void testCalculateSimpleExampleCaseInsensitive() throws Exception {
+        Counter cnt = new Counter();
+        String text = "Lorem ipsum ad his scripta blandit partiendo. Scripta ad His.";
+        Hashtable<String, Integer> retVal;
+        cnt.setCaseSensitive(false);
+        retVal = cnt.calculate(text);
 
+        Assert.assertEquals(2, retVal.get("ad").intValue());
+        Assert.assertEquals(2, retVal.get("his").intValue());
+        Assert.assertNull(retVal.get("Scripta"));
+        Assert.assertEquals(2, retVal.get("scripta").intValue());
+        Assert.assertEquals(1, retVal.get("partiendo").intValue());
+    }
+
+
+    @Test
+    public void testCalculateWithIgnoreListExampleCaseSensitive() throws Exception {
+        ArrayList<String> ignoreList = new ArrayList<String>();
+        ignoreList.add("His");
+        ignoreList.add("$\\wd^");
+        ignoreList.add("$L.+sum");
+        Counter cnt = new Counter(ignoreList);
+        String text = "Lorem ipsum ad his scripta blandit partiendo. Scripta ad His.";
+        Hashtable<String, Integer> retVal;
+        retVal = cnt.calculate(text);
+        cnt.setCaseSensitive(true);
+        Assert.assertNull(retVal.get("ad"));
+        Assert.assertNull(retVal.get("Lorem"));
+        Assert.assertEquals(1, retVal.get("his").intValue());
+        Assert.assertEquals(1, retVal.get("Scripta").intValue());
+    }
+    @Test
+    public void testCalculateWithIgnoreListExampleCaseInsensitive() throws Exception {
+        ArrayList<String> ignoreList = new ArrayList<String>();
+        ignoreList.add("His");
+        ignoreList.add("$\\wd^");
+        ignoreList.add("$L.+sum");
+        Counter cnt = new Counter();
+        String text = "Lorem ipsum ad his scripta blandit partiendo. Scripta ad His.";
+        Hashtable<String, Integer> retVal;
+        cnt.setCaseSensitive(false);
+        retVal = cnt.calculate(text);
+
+        Assert.assertEquals(2, retVal.get("his").intValue());
+        Assert.assertNull(retVal.get("ad"));
+        Assert.assertNull(retVal.get("lorem"));
+        Assert.assertNull(retVal.get("Scripta"));
+        Assert.assertEquals(2, retVal.get("scripta").intValue());
+        Assert.assertEquals(1, retVal.get("partiendo").intValue());
     }
 }
